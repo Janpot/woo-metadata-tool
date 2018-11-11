83b0b0f255b2afa4d2cf872136c261265daaad41
Jan Potoms <potoms.jan@gmail.com>
2018-11-11 17:45:50 +0100

    Updates

---
+++ precache-manifest.0e0572323cd92f826637f0489b3b00cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4954ea88925c791ee37",
+    "url": "/woo-metadata-tool/static/css/main.0f01fc6b.chunk.css"
+  },
+  {
+    "revision": "d4954ea88925c791ee37",
+    "url": "/woo-metadata-tool/static/js/main.d4954ea8.chunk.js"
+  },
+  {
+    "revision": "ae3db91c8f4877a5986a",
+    "url": "/woo-metadata-tool/static/css/1.139b14bc.chunk.css"
+  },
+  {
+    "revision": "ae3db91c8f4877a5986a",
+    "url": "/woo-metadata-tool/static/js/1.ae3db91c.chunk.js"
+  },
+  {
+    "revision": "3abdb536be39b093a9a3",
+    "url": "/woo-metadata-tool/static/js/runtime~main.3abdb536.js"
+  },
+  {
+    "revision": "257af853fa54bca288873f365d19f4cf",
+    "url": "/woo-metadata-tool/static/media/github-logo.257af853.svg"
+  },
+  {
+    "revision": "29dc8ccafdfdf335b5f74d2da0789dd7",
+    "url": "/woo-metadata-tool/static/media/woorank-logo.29dc8cca.svg"
+  },
+  {
+    "revision": "74d30aff4a5047528c95ac4bef0e81d6",
+    "url": "/woo-metadata-tool/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/woo-metadata-tool/precache-manifest.0e0572323cd92f826637f0489b3b00cb.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/woo-metadata-tool/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
