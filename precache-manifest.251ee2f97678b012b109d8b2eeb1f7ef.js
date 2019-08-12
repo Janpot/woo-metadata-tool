a809279740093409f587e6da0da78e63db498223
Jan Potoms <potoms.jan@gmail.com>
2019-08-12 22:39:20 +0200

    Updates

---
+++ precache-manifest.251ee2f97678b012b109d8b2eeb1f7ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "29dc8ccafdfdf335b5f74d2da0789dd7",
+    "url": "/woo-metadata-tool/static/media/woorank-logo.29dc8cca.svg"
+  },
+  {
+    "revision": "257af853fa54bca288873f365d19f4cf",
+    "url": "/woo-metadata-tool/static/media/github-logo.257af853.svg"
+  },
+  {
+    "revision": "3abdb536be39b093a9a3",
+    "url": "/woo-metadata-tool/static/js/runtime~main.3abdb536.js"
+  },
+  {
+    "revision": "e0779ed5a7f97ed9bc3a",
+    "url": "/woo-metadata-tool/static/js/main.e0779ed5.chunk.js"
+  },
+  {
+    "revision": "a3b77647b73c79ef3398",
+    "url": "/woo-metadata-tool/static/js/1.a3b77647.chunk.js"
+  },
+  {
+    "revision": "e0779ed5a7f97ed9bc3a",
+    "url": "/woo-metadata-tool/static/css/main.2e31f15b.chunk.css"
+  },
+  {
+    "revision": "a3b77647b73c79ef3398",
+    "url": "/woo-metadata-tool/static/css/1.143692f0.chunk.css"
+  },
+  {
+    "revision": "2c8f0d87cdc1aeb2d52064d49a170e20",
+    "url": "/woo-metadata-tool/index.html"
+  }
+];
