ba9146ab93409dfe5358bb210b818a0f45048167
Jan Potoms <potoms.jan@gmail.com>
2019-08-18 13:43:09 +0200

    Bring back gogole analytics

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,10 @@ import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../src/theme';
+import Router from 'next/router';
+import * as gtag from '../src/gtag';
+
+Router.events.on('routeChangeComplete', url => gtag.pageview(url));
 
 class MyApp extends App {
   componentDidMount() {
--- pages/_document.js
+++ pages/_document.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/styles';
 import theme from '../src/theme';
+import { GA_TRACKING_ID } from '../src/gtag';
 
 class MyDocument extends Document {
   render() {
@@ -19,6 +20,21 @@ class MyDocument extends Document {
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?display=swap&family=Roboto:300,400,500,700"
+          />
+
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_TRACKING_ID}');
+              `
+            }}
           />
         </Head>
         <body>
